f675499aac22a3f7f2472c8305f508fe24023bf5
7ua <80481493+7ua@users.noreply.github.com>
2021-10-17 15:03:14 -0700

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,11 +1,11 @@
 #!bin/bash
-echo "Welcome to the Moon updater. Moon will now update."
+echo "Welcome to the Mooncord updater. Mooncord will now update."
 sleep 2
 rm -rf Moon
 echo "Removed old version"
 sleep 1
 echo "Installing new version"
 sleep 3
-git clone https://github.com/7ua/Moon && cd Moon && mv moon.sh ~ && mv update.sh ~ && cd 
+git clone https://github.com/7ua/Mooncord && cd Moon && mv moon.sh ~ && mv update.sh ~ && cd 
 clear
 echo "Moon was updated & re-installed."

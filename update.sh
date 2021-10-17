9bb59ab00319e11ebffca209458a336be94d84b8
7ua <80481493+7ua@users.noreply.github.com>
2021-10-16 17:14:45 -0700

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,8 +1,11 @@
 #!bin/bash
 echo "Welcome to the Moon updater. Moon will now update."
+sleep 2
 rm -rf Moon
 echo "Removed old version"
+sleep 1
 echo "Installing new version"
+sleep 3
 git clone https://github.com/7ua/Moon && cd Moon && mv moon.sh ~ && mv update.sh ~ && cd 
   ##############################
   ############################

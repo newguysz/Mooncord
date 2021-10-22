dbc94017e53768294e9ccc847a25a04b77e2e384
7ua <80481493+7ua@users.noreply.github.com>
2021-10-22 05:29:55 -0700

    Update new.py

--- Moon-1.0.1/versions/new.py
+++ Moon-1.0.1/versions/new.py
@@ -36,7 +36,9 @@ if numbersOnly == True:
 if lnmix == True:
   a = ['1','2','3','4','5','6','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','7','8','9','0']
 print(Fore.RED + logo)
-print(" Enter 1. to use Nitro gen & checker. \n Enter 2. to use code gen (no checker) \n Enter 3. to use invite gen (no checker) \n Enter 4. to use invite gen & checker.")
+options = [' Enter 1. to use Nitro gen & checker. ','Enter 2. to use code gen (no checker) ', 'Enter 3. to use invite gen (no checker) ', 'Enter 4. to use invite gen & checker.']
+choice = enquiries.choose('Choose one of these options: ', options)
+#print(" Enter 1. to use Nitro gen & checker. \n Enter 2. to use code gen (no checker) \n Enter 3. to use invite gen (no checker) \n Enter 4. to use invite gen & checker.")
 print(" Enter ? help")
 choice = input(Fore.YELLOW+"[-]: ")
 if choice == "1":                                                                                   

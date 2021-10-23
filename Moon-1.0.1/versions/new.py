f3fe62e1a31d071db8b4e542986b9575caf2ddd4
7ua <80481493+7ua@users.noreply.github.com>
2021-10-22 17:58:45 -0700

    Update new.py

--- Moon-1.0.1/versions/new.py
+++ Moon-1.0.1/versions/new.py
@@ -1,4 +1,6 @@
-#  Github.com/7ua                                                      
+#  Github.com/7ua  
+
+# PACKAGES #
 import random 
 import time
 import requests
@@ -7,7 +9,13 @@ import sys
 import enquiries
 from colorama import Fore, Back, Style
 from config import *
+# PACKAGES #
+
+# CLEAR PROMPT #
 os.system("clear")
+# CLEAR PROMPT #
+
+# STARTUP #
 print("Mooncord. Made by Github.com/7ua. Mooncord will now start in a second.")
 time.sleep(0.9)
 os.system('clear')
@@ -30,17 +38,27 @@ logo = ("""
   Made by: Github.com/7ua. Orginal repo: Github.com/7ua/Moon.
   
 """)
+# STARTUP #
+
+#  LOADING SETTINGS #
 if lettersOnly == True:
  a=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 if numbersOnly == True:
   a=['1','2','3','4','5','6','7','8','9','0']
 if lnmix == True:
   a = ['1','2','3','4','5','6','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','7','8','9','0']
+#  LOADING SETTINGS #
+
+#MAIN MENU#
 print(Fore.RED + logo)
 options = ['\033[31m'+'Nitro gen & checker. ', '\033[31m'+'Nitro gen (no checker) ', '\033[31m'+'invite gen (no checker) ', '\033[31m'+'invite gen & checker.', '\033[31m'+'exit']
 choice = enquiries.choose('Choose one of these options: ', options)
+# MAIN MENU #
+
+# OP 1 #
 if choice == "\033[31mNitro gen & checker. ":
- while True:                                                                     
+ while True:                              
+  # CREATE CODE #
    w = random.choice(a)                                                           
    d = random.choice(a)                                                           
    e = random.choice(a)                                                           
@@ -57,9 +75,10 @@ if choice == "\033[31mNitro gen & checker. ":
    q = random.choice(a) 
    r = random.choice(a) 
    s = random.choice(a) 
-   m = f+e+w+d+g+h+i+j+k+l+n+o+p+q+r+s                                                            
+   m = f+e+w+d+g+h+i+j+k+l+n+o+p+q+r+s    
+   # CREATE CODE #
    c = 'https://discordapp.com/api/v6/entitlements/gift-codes/'+m+'?with_application=false&with_subscription_plan=true'
-                                                                                  
+    #CHECK CODE#                                                                              
    b = requests.get(c)                                                            
    if b.status_code == 404:                                                       
                                                                                   
@@ -67,7 +86,7 @@ if choice == "\033[31mNitro gen & checker. ":
    if b.status_code == 429:                                                       
     print(Fore.WHITE+Back.YELLOW+"[WARNING]"+Style.RESET_ALL+"\033[1;33m"+"[-]: Rate Limited | Waiting 40 Seconds | Webhook:On | Webhook Alerted! | "+"\033[0m")
     time.sleep(39.9)
-                                                                                   
+        #SNIPER OPTION#                                                                           
    if b.status_code == 200:                                                       
     print('[GOOD] [+]: discord.gifts/'+m+'Is Valid!')
     if sniperOn == True:
@@ -78,7 +97,10 @@ if choice == "\033[31mNitro gen & checker. ":
        file.write('discord.gifts/'+m)                                               
        file.close()
        print("Code saved in 'okcodes.txt'")
+       #SNIPER OPTION# 
+       #CHECK CODE#  
        
+       #REST OF THE OP#
 if choice == "\033[31mNitro gen (no checker) ":
  os.system('python3 gen1.py')
 if choice == "\033[31minvite gen (no checker) ":

27dd3243456455f127684b3ab072e446b79dc62b
7ua <80481493+7ua@users.noreply.github.com>
2021-11-08 18:26:18 -0800

    Update tokeng.py

--- Moon-1.0.1/versions/tokeng.py
+++ Moon-1.0.1/versions/tokeng.py
@@ -1,6 +1,7 @@
 import string 
 import random
+import timwe
 
 while True:
-  sleep('0.2')
+  time.sleep('0.2')
   print(random.choice(string.ascii_letters)+''.join(random.choice(string.ascii_letters + string.digits) for _ in range(20))+"."+random.choice(string.ascii_letters).upper()+''.join(random.choice(string.ascii_letters + string.digits) for _ in range(5))+"."+''.join(random.choice(string.ascii_letters + string.digits) for _ in range(27)))

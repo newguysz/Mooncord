8e4ab1029eb600f3249131553d28fd9d489d10dc
7ua <80481493+7ua@users.noreply.github.com>
2021-11-08 17:44:25 -0800

    Update mcl.sh

--- mcl.sh
+++ mcl.sh
@@ -1,1 +1,11 @@
-
+import os
+start=input('Launch on startup installation y or n')
+print('Installing required packages.')
+os.system('pip3 install random')
+os.system('pip3 install time')
+os.system('pip3 install requests')
+os.system('pip3 install sys')
+os.system('pip3 install enquiries')
+os.system('pip3 install colorama')
+if start == 'y':
+ os.system('bash mooncord.sh')
